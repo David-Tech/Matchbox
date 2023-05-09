96f2110f33e53a947380c61786c25b17a2aff369
Denise-1 <denisechec@gmail.com>
2023-05-09 13:55:40 -0400

    settings add more dropdown

--- client/src/components/pages/Settings.js
+++ client/src/components/pages/Settings.js
@@ -2,36 +2,52 @@ import React, { useState } from 'react';
 import '../CSS/settings.css';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown,  faUser } from '@fortawesome/free-solid-svg-icons';
 
 function Settings() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-    dropdownMenu.classList.toggle('show');
   };
 
+  const toggleDropdown2 = () => {
+    setIsOpen2(!isOpen2);
+  };
+
+  // <FontAwesomeIcon icon={faUser} /> 
   return (
     <div className="sidebar">
       <ul>
         <li className="dropdown">
-          <button className="sidebar-btn" onClick={toggleDropdown}>
-            Dropdown <FontAwesomeIcon icon={faCaretDown} />
+        <button className="sidebar-btn" onClick={toggleDropdown}>Account <FontAwesomeIcon icon={faCaretDown} />
           </button>
-          <ul className="dropdown-menu">
+          <ul  className={`dropdown-menu ${isOpen ? 'show' : ''}`} >
             <li>
-              <a href="#">Option 1</a>
+              <a  href="#" className ="sub-item">Personal Infomation</a>
             </li>
+    
             <li>
-              <a href="#">Option 2</a>
-            </li>
-            <li>
-              <a href="#">Option 3</a>
+              <a href="#" className ="sub-item">Notification</a>
             </li>
           </ul>
         </li>
+
+        <li className={`dropdown ${isOpen ? 'shift-down' : ''}`}>
+          <button className="sidebar-btn " onClick={toggleDropdown2}>
+            Subscription <FontAwesomeIcon icon={faCaretDown} />
+          </button>
+          <ul className={`dropdown-menu ${isOpen2 ? 'show' : ''}`}>
+            <li>
+              <a href="#" className ="sub-item">Subscription Plan</a>
+            </li>
+            <li>
+              <a href="#" className ="sub-item">Billing</a>
+            </li>
+          </ul>
+        </li>
+
       </ul>
     </div>
   );
