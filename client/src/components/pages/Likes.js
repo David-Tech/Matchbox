690f181a2f68db37a82e8f555ebe682be531243e
Denise-1 <denisechec@gmail.com>
2023-05-11 15:48:53 -0400

    likes layout placeholder

--- client/src/components/pages/Likes.js
+++ client/src/components/pages/Likes.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import '../CSS/likes.css';
 
 function Likes() {
   return (
-    <div>Likes</div>
+      <div className="likes-body"> 
+        <p>No Likes yet</p>
+        <p><Link className="sign-color" to="/Matchbox/upload"> Upload</Link> a picture to get started!</p>
+      </div>
   )
 }
 
