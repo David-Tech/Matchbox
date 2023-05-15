f813880ffca9fdca0157b999b7d72d2d35520d77
David Briken <dbriken.tech@gmail.com>
2023-05-14 23:35:01 -0400

    Added link to User Documentation

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -6,9 +6,13 @@ function Home() {
   return (
   <div className="body-home">
     <h1>Welcome to Matchbox</h1>
+
     <p><Link className="sign-color" to="/Matchbox/signup">Sign up</Link></p>
     <p>or</p>
     <p><Link className="sign-color" to="/Matchbox/login">Login</Link></p>
+    <br />
+    <br />
+    <a target="_blank" href="https://docs.google.com/presentation/d/1nlvxlSqlyKIxcrMWUEHL_s5ZkkbAApBC/edit?usp=sharing&ouid=112945152730092009592&rtpof=true&sd=true">User Documentation</a>
   </div>  
   )
 }
